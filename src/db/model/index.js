1112b94e79ebdc7301ae6d7c8562aa3be63065a8
even-lq <lqnwd999@163.com>
2022-08-15 23:31:32 +0800

    feat:粉丝数据模型

--- src/db/model/index.js
+++ src/db/model/index.js
@@ -4,13 +4,23 @@
 
 const User = require('./User');
 const Blog = require('./Blog');
+const UserRelation = require('./UserRelation');
 
 // 创建Blog的外键
 Blog.belongsTo(User, {
   foreignKey: 'userId'
 });
 
+UserRelation.belongsTo(User, {
+  foreignKey: 'followerId'
+});
+
+User.hasMany(UserRelation, {
+  foreignKey: 'userId'
+});
+
 module.exports = {
   User,
-  Blog 
+  Blog,
+  UserRelation
 };
